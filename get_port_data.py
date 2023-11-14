afc9def431886547d10f0a239a35da923e396d5a
Cat Gurinsky <karate@apple.com>
2023-11-14 09:54:22 -0700

    update to rich tables

--- get_port_data.py
+++ get_port_data.py
@@ -15,14 +15,11 @@
 try:
     # importing libraries
     import argparse
-    import datetime
-    import logging
     import os
     import re
-    import sys
-    from getpass import getpass
 
-    from prettytable import PrettyTable
+    from rich.console import Console
+    from rich.table import Table
 
     import netlib
 except ImportError as error:
@@ -63,6 +60,7 @@ start_logger = netlib.setup_logging("port_data", output_dir)
 logger = start_logger[0]
 logger_full_path = start_logger[1]
 
+console = Console()
 
 eapi = netlib.AristaPyeapi(username, password, switch_hostname, logger)
 
@@ -81,48 +79,46 @@ else:
 
 def show_switch_version():
     show_version = eapi.get_version()
-    switch_version_table = PrettyTable()
-    switch_version_table.field_names = [
-        "Hostname",
-        "Model",
-        "OS Version",
-        "Serial Number",
-    ]
+    switch_version_table = Table()
+    switch_version_table.add_column(
+        "Hostname", justify="left", style="magenta", no_wrap=True, vertical="middle"
+    )
+    switch_version_table.add_column("Model", justify="center", no_wrap=True)
+    switch_version_table.add_column("OS\nVersion", justify="center", no_wrap=True)
+    switch_version_table.add_column("Serial\nNumber", justify="center", no_wrap=True)
+
     switch_version_table.add_row(
-        [
-            hostname_short,
-            show_version["model"],
-            show_version["eos_version"],
-            show_version["serial_number"],
-        ]
+        hostname_short,
+        show_version["model"],
+        show_version["eos_version"],
+        show_version["serial_number"],
     )
     print("")
-    print(switch_version_table)
+    console.print(switch_version_table)
 
 
 def interface_inventory(base_port, port_name_long):
     get_interface_inventory = eapi.get_inventory("interfaces")[base_port]
     inventory_vendor = get_interface_inventory["mfgName"]
-    port_inventory_table = PrettyTable()
-    port_inventory_table.field_names = [
-        "Hostname",
-        "Port",
-        "Manufacturer",
-        "Model",
-        "Serial Number",
-    ]
+    port_inventory_table = Table()
+    port_inventory_table.add_column(
+        "Hostname", justify="left", style="magenta", no_wrap=True, vertical="middle"
+    )
+    port_inventory_table.add_column("Port", justify="center", no_wrap=True)
+    port_inventory_table.add_column("Manufacturer", justify="center", no_wrap=True)
+    port_inventory_table.add_column("Model", justify="center", no_wrap=True)
+    port_inventory_table.add_column("Serial\nNumber", justify="center", no_wrap=True)
+
     port_inventory_table.add_row(
-        [
-            hostname_short,
-            port_name_long,
-            inventory_vendor,
-            get_interface_inventory["modelName"],
-            get_interface_inventory["serialNum"],
-        ]
+        hostname_short,
+        port_name_long,
+        inventory_vendor,
+        get_interface_inventory["modelName"],
+        get_interface_inventory["serialNum"],
     )
     print("")
     print(f"{hostname_short} # show inventory | inc {port_name}")
-    print(port_inventory_table)
+    console.print(port_inventory_table)
     return inventory_vendor
 
 
@@ -130,58 +126,55 @@ def interface_errors(port_name_long):
     get_errors = eapi.try_eapi_command("show interfaces counters errors", "enable")[
         "interfaceErrorCounters"
     ][port_name_long]
-    interface_counters_table = PrettyTable()
-    interface_counters_table.field_names = [
-        "Port",
-        "FCS",
-        "Align",
-        "Symbol",
-        "Rx",
-        "Runts",
-        "Giants",
-        "Tx",
-    ]
+    interface_counters_table = Table()
+    interface_counters_table.add_column(
+        "Port", justify="left", style="magenta", no_wrap=True
+    )
+    interface_counters_table.add_column("FCS", justify="center", no_wrap=True)
+    interface_counters_table.add_column("Align", justify="center", no_wrap=True)
+    interface_counters_table.add_column("Symbol", justify="center", no_wrap=True)
+    interface_counters_table.add_column("RX", justify="center", no_wrap=True)
+    interface_counters_table.add_column("Runts", justify="center", no_wrap=True)
+    interface_counters_table.add_column("Giants", justify="center", no_wrap=True)
+    interface_counters_table.add_column("TX", justify="center", no_wrap=True)
+
     interface_counters_table.add_row(
-        [
-            port_name_long,
-            get_errors["fcsErrors"],
-            get_errors["alignmentErrors"],
-            get_errors["symbolErrors"],
-            get_errors["inErrors"],
-            get_errors["frameTooShorts"],
-            get_errors["frameTooLongs"],
-            get_errors["outErrors"],
-        ]
+        port_name_long,
+        str(get_errors["fcsErrors"]),
+        str(get_errors["alignmentErrors"]),
+        str(get_errors["symbolErrors"]),
+        str(get_errors["inErrors"]),
+        str(get_errors["frameTooShorts"]),
+        str(get_errors["frameTooLongs"]),
+        str(get_errors["outErrors"]),
     )
     print(f"\n{hostname_short}# show interfaces counters errors")
-    print(interface_counters_table)
+    console.print(interface_counters_table)
 
 
 def interface_transceiver(port_name_long, dash2, dash3, dash4):
     get_interface_transceiver = eapi.try_eapi_command(
         "show interfaces transceiver", "enable"
     )["interfaces"]
-    transceiver_results_table = PrettyTable()
-    transceiver_results_table.field_names = [
-        "Port",
-        "Temp(C)",
-        "Voltage",
-        "Bias Current",
-        "Tx (dBm)",
-        "Rx (dBm)",
-    ]
+    transceiver_results_table = Table()
+    transceiver_results_table.add_column(
+        "Port", justify="left", style="magenta", no_wrap=True
+    )
+    transceiver_results_table.add_column("Temp(C)", justify="center", no_wrap=True)
+    transceiver_results_table.add_column("Voltage", justify="center", no_wrap=True)
+    transceiver_results_table.add_column("Bias Current", justify="center", no_wrap=True)
+    transceiver_results_table.add_column("Tx (dBm)", justify="center", no_wrap=True)
+    transceiver_results_table.add_column("Rx (dBm)", justify="center", no_wrap=True)
 
     def get_transceiver(port):
         levels = get_interface_transceiver[port]
         transceiver_results_table.add_row(
-            [
-                port,
-                levels["temperature"],
-                levels["voltage"],
-                levels["txBias"],
-                levels["txPower"],
-                levels["rxPower"],
-            ]
+            port,
+            f'{levels["temperature"]:.2f}',
+            f'{levels["voltage"]:.2f}',
+            f'{levels["txBias"]:.2f}',
+            f'{levels["txPower"]:.2f}',
+            f'{levels["rxPower"]:.2f}',
         )
 
     get_transceiver(port_name_long)
@@ -192,7 +185,7 @@ def interface_transceiver(port_name_long, dash2, dash3, dash4):
     print("")
     print(f"{hostname_short}# show interfaces transceiver")
     transceiver_results_table.float_format = ".2"
-    print(transceiver_results_table)
+    console.print(transceiver_results_table)
 
 
 def mac_details(port_name):
@@ -213,13 +206,16 @@ current_port_line_protocol = current_port_status["lineProtocolStatus"]
 if not results_lldp:
     # if LLDP returned no data, print an error to the screen and the log file
     print(
-        f"\nATTN: {port_name} has no LLDP neighbor. Current description: {current_port_desc}"
+        f"\nATTN: {port_name} has no LLDP neighbor. "
+        f"Current description: {current_port_desc}"
     )
     print(
-        f"Current link status: {current_port_link_status} line protocol status: {current_port_line_protocol}"
+        f"Current link status: {current_port_link_status} line protocol "
+        f"status: {current_port_line_protocol}"
     )
     logger.warning(
-        f"{switch_hostname} {port_name} has no LLDP neighbor. Current description: {current_port_desc}"
+        f"{switch_hostname} {port_name} has no LLDP neighbor. "
+        f"Current description: {current_port_desc}"
     )
 else:
     # get the neighbor information from LLDP
@@ -227,18 +223,18 @@ else:
     my_neighbor_port = my_neighbor_port_long.replace("Ethernet", "Et")
     my_neighbor_port = my_neighbor_port_long.replace("Management", "Ma")
     my_neighbor_device = results_lldp[0]["neighborDevice"]
-    lldp_neighbor_table = PrettyTable()
-    lldp_neighbor_table.field_names = [
-        "Hostname",
-        "Local Port",
-        "LLDP Neighbor",
-        "Remote Port",
-    ]
+    lldp_neighbor_table = Table()
+    lldp_neighbor_table.add_column(
+        "Hostname", justify="left", style="magenta", no_wrap=True, vertical="middle"
+    )
+    lldp_neighbor_table.add_column("Local Port", justify="center")
+    lldp_neighbor_table.add_column("LLDP Neighbor", justify="left")
+    lldp_neighbor_table.add_column("Remote Port", justify="center")
     lldp_neighbor_table.add_row(
-        [hostname_short, port_name_long, my_neighbor_device, my_neighbor_port_long]
+        hostname_short, port_name_long, my_neighbor_device, my_neighbor_port_long
     )
     print(f"{hostname_short}# show lldp neighbors")
-    print(lldp_neighbor_table)
+    console.print(lldp_neighbor_table)
 
 print("\n**************************************************************************")
 show_switch_version()
